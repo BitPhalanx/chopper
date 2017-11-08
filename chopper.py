db4894d4577d3ab2a14d78d4b89662bed5d6710d
jcarye <joshuacaryer@gmail.com>
2017-11-08 17:51:28 -0500

    First Update
    Add controls, access to incoming data, access to steps and resetting environment after done flag is set. Still a lot to do, no neural network yet...

--- chopper.py
+++ chopper.py
@@ -22,5 +22,58 @@ import numpy as np
 import random
 import math
 
-# Pretend there's code here actually doing something ahha
-# I'm still researching
+# def cnn_model():
+  # We will create the model for our CNN here
+  # The plan: Input layer takes in 105x80x3 = 25200
+  # 
+
+def choose_action():
+  # Feed in probability and return an action 
+  # Actions: up, down, left, right, shoot, nothing
+  #           2     5     4      3      1        0
+  # Fix this return after neural network is created
+  return random.randint(1,6)
+
+def main():
+  # Prepare Chopper Command as env variable
+  # and start access to images
+  env = gym.make('ChopperCommand-v0')
+  observation = env.reset()
+
+  # observation now holds unsigned 8 bit int array
+  # with shape (210, 160, 3). Let's half this for
+  # our neural network to allow easier processing
+  # by taking every other pixel
+  reduced_observation = observation[::2, ::2, :]
+  # reduced_observation is now shape (105,80,3)
+  # Confirm reduced observation shape
+  # print("Reduced observation shape: ", reduced_observation.shape)
+
+  # Choosing to keep colors since enemies have different
+  # colors. We can now feed this into our CNN.
+  # Blue planes and white helicoptors = enemies
+  # Black trucks = friendly
+
+  # We need int32s, so cast to this type and double check
+  # casted_observation = tf.cast(reduced_observation, tf.float32) / 255.0
+  # casted_observation = tf.cast(reduced_observation, tf.float32)
+  # print(observation)
+  # print(casted_observation)
+
+  # Reshape our array into 4-D tensor to allow input to NN  
+  # input_layer = tf.reshape(reduced_observation, [-1,105,80,3])
+  # print("Input Layer shape: ", input_layer.shape)
+
+  #env.render allows us to render the graphics
+  while True:
+    observation, reward, done, info = env.step(choose_action())
+    print(observation)
+    env.render()
+    # env.step()
+    if done:
+      # Compute weight changes and backpropagate here
+      # Then reset the environment for another run.
+      env.reset()
+
+if __name__ == "__main__":
+  main()
