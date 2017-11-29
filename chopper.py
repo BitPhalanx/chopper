ffc72b57c04677c68e02ac301d5dbfea5a9ade05
jcarye <joshuacaryer@gmail.com>
2017-11-29 17:58:14 -0500

    Update
    Add batch norm, outline for multi-filters, etc

--- chopper.py
+++ chopper.py
@@ -23,47 +23,88 @@ import random
 # import math
 import time
 
-def cnn_model(x):
+# def policy_gradient():
+#   with tf.variable_scope("policy"):
+
+# def value_gradient():
+#   with tf.variable_scope("value"):
+
+def cnn_model(x, bn_is_training):
+  # Batch Norm HyperParameters
+  # bn_is_training = True
+  bn_scale = True
+
   # We will create the model for our CNN here
   # Input layer takes in 104x80x3 = 25200
   with tf.name_scope('reshape'):
     x_image = tf.reshape(x, [-1, 104, 80, 3])
+    # print(x_image)
 
-  # Conv 10x10 box across 3 color channels into 32 features
+  # Conv 3x3 box across 3 color channels into 32 features
   with tf.name_scope('conv1'):
-    W_conv1 = weight_variable([10,10,3,32])
+    W_conv1 = weight_variable([3,3,3,32])
     b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1) + b_conv1)
+    pre_bn_conv1 = conv2d(x_image, W_conv1) + b_conv1
+    post_bn_conv1 = tf.contrib.layers.batch_norm(pre_bn_conv1, center = True, scale = bn_scale, is_training = bn_is_training, scope = 'bn1')
+    h_conv1 = tf.nn.relu(post_bn_conv1)
+    # print(tf.shape(h_conv1))
+    # print(h_conv1) shows shape = (1,104,80,32)
+
+  # Not sure if doing this right, but adding a 2nd 3x3 filter...?
+  # W_conv1_2 = weight_variable([3,3,3,32])
+  # b_conv1_2 = bias_variable([32])
+  # pre_bn_conv1_2 = conv2d(x_image, W_conv1_2) + b_conv1_2
+  # post_bn_conv1_2 = tf.contrib.layers.batch_norm(pre_bn_conv1_2, center = True, scale = bn_scale, is_training = bn_is_training, scope = 'bn1_2')
+  # h_conv1_2 = tf.nn.relu(post_bn_conv1_2)
+
+  # Now, combine these two tensors? Should they be combined?
+  # Before or after maxpool?
+  # h_conv1_combined = tf.concat([h_conv1, h_conv1_2], axis_to_combine = 3)
 
   # Max pool to half size (52x40)
   with tf.name_scope('pool1'):
     h_pool1 = max_pool_2x2(h_conv1)
 
-  # 2nd conv, 10x10 box from 32 to 64 features
+  # 2nd conv, 3x3 box from 32 to 64 features
   with tf.name_scope('conv2'):
-    W_conv2 = weight_variable([10,10,32,64])
+    W_conv2 = weight_variable([3,3,32,64])
     b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    pre_bn_conv2 = conv2d(h_pool1, W_conv2) + b_conv2
+    post_bn_conv2 = tf.contrib.layers.batch_norm(pre_bn_conv2, center = True, scale = bn_scale, is_training = bn_is_training, scope = 'bn2')
+    h_conv2 = tf.nn.relu(post_bn_conv2)
 
   # 2nd max pool, half size again (26x20)
   with tf.name_scope('pool2'):
     h_pool2 = max_pool_2x2(h_conv2)
 
-  # Start fully connected layer, 26*20*64 = 33280 to 8192 fully connected
+  # 3rd conv, 3x3 box from 64 to 128 features
+  with tf.name_scope('conv3'):
+    W_conv3 = weight_variable([3,3,64,128])
+    b_conv3 = bias_variable([128])
+    pre_bn_conv3 = conv2d(h_pool2, W_conv3) + b_conv3
+    post_bn_conv3 = tf.contrib.layers.batch_norm(pre_bn_conv3, center = True, scale = bn_scale, is_training = bn_is_training, scope = 'bn3')
+    h_conv3 = tf.nn.relu(post_bn_conv3)
+
+  # 3rd max pool, half size last time (13x10)
+  with tf.name_scope('pool3'):
+    h_pool3 = max_pool_2x2(h_conv3)
+    print(h_pool3)
+
+  # First fully connected layer, 13*10*128 = 16640 to 512 fully connected
   with tf.name_scope('fc1'):
-    W_fc1 = weight_variable([26*20*64, 8192])
-    b_fc1 = bias_variable([8192])
+    W_fc1 = weight_variable([13*10*128, 512])
+    b_fc1 = bias_variable([512])
     # Flatten max pool to enter fully connected layer
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 26*20*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_pool3_flat = tf.reshape(h_pool3, [-1, 13*10*128])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
   with tf.name_scope('dropout'):
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-  # Fully connected from 8192 to 6 (1 for each action possible)
+  # Fully connected from 512 to 6 (1 for each action possible)
   with tf.name_scope('fc2'):
-    W_fc2 = weight_variable([8192, 6])
+    W_fc2 = weight_variable([512, 6])
     b_fc2 = bias_variable([6])
 
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
@@ -111,7 +152,7 @@ def main():
   print("Reduced observation shape: ", reduced_observation.shape)
   float_input = reduced_observation.astype(np.float32)
   # reduced_observation.view('<f4')
-  y_conv, keep_prob = cnn_model(float_input)
+  y_conv, keep_prob = cnn_model(float_input, True)
   print("Keep_prob: ", keep_prob)
   print("Y_Conv: ", y_conv)
 
